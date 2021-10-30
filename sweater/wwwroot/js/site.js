9946dfaa45a4bcd56462a8100e65dc226b408915
tnye02 <tnye02@gmail.com>
2021-10-29 21:44:54 -0700

    Initial commit

---
+++ sweater/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+var string = 'Please enjoy this video for "Undone - The Sweater Song" by Weezer.';
+var str = string.split("");
+var el = document.getElementById('str');
+
+(function animate() {
+    str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running);
+    var running = setTimeout(animate, 90);
+})();
